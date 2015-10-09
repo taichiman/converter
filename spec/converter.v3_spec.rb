d6dee314dc2c5a5152335791f8527b92cd7a3115
Denis Krivostchokov <densomart@gmail.com>
2015-10-09 19:22:33 +0600

    refactoring of tests

--- spec/converter.v3_spec.rb
+++ spec/converter.v3_spec.rb
@@ -1,27 +1,30 @@
-require_relative '../lib/convertor.v3.rb'
+require 'convertor.v3'
 
-describe 'Converter' do
-  it 'test whole script' do
-    allow(self).to receive(:download_all) do
-      file = File.new(File.join(Dir.pwd,'spec/stuff','doc1.xml'), 'w')
-      file.close
+STUFF_DIR  = File.join(Dir.pwd,'spec/stuff')
+
+describe 'Converter script' do
+  context 'the script test' do
+    it 'test main workflow' do
+      allow(self).to receive(:download_all) do
+        file = File.new(File.join(STUFF_DIR,'doc1.xml'), 'w')
+        file.close
+      end
+
+      run
+
+      expect(self).to have_received(:download_all).once
+      expect(File.exist?(File.join(STUFF_DIR,'doc1.xml'))).to eq true
 
     end
-
-    #%x[ruby converter.v3.rb 'http://example.file']
-
-    run
-
-    expect(self).to have_received(:download_all).once
-
-    expect(File.exist?(File.join(Dir.pwd,'spec/stuff','doc1.xml'))).to eq true
-
   end
 
+end
+
+describe 'test downloading' do
   context 'can download all documents' do
     #download_all_document(url)
     it 'download 1 doc' do
-      expect(Dir[File.join(Dir.pwd,'spec/stuff','**','*')].count{ |file| File.file? file }).to eq 1
+      expect(Dir[File.join(STUFF_DIR,'**','*')].count{ |file| File.file? file }).to eq 1
     end
 
   end
